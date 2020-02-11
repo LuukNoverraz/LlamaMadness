bb1a1950df8537b2fd31ab82da63f3b6452076aa
LuukNoverraz <luuk.noverraz@coderclass.nl>
2020-02-11 21:26:27 +0100

    [ADD] First concept of third person multiplayer racing

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    [SerializeField] private Transform[] playerTransforms;
+    private Vector3 cameraPosition;
+    private float playerDistance;
+
+    void Update()
+    {
+        playerDistance = ((playerTransforms[0].position.z + playerTransforms[1].position.z) / 2) - 4;
+        transform.position = new Vector3(0, 2.7f, playerDistance);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private string[] inputs;
     private Rigidbody rb;
     [SerializeField] private float jumpForce;
     private Vector3 jump;
     private bool grounded;
+    [SerializeField] private Transform cameraTransform;
 
     void Start()
     {
@@ -15,23 +17,15 @@ public class PlayerController : MonoBehaviour
         jump = new Vector3(0.0f, jumpForce, 0.0f);
         Cursor.lockState = CursorLockMode.Locked;
     }
-    
-    void OnCollisionStay()
-    {
-        grounded = true;
-    }
+
+    void OnCollisionEnter(Collision col) => grounded = true;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        if (Input.GetKeyDown(KeyCode.E) && grounded)
         {
             grounded = false;
             rb.velocity = jump;
-        }
-
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-
         }
 
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -44,6 +38,6 @@ public class PlayerController : MonoBehaviour
     {
         // float moveHorizontal = -Input.GetAxisRaw("LeftJoyStickHorizontal");
         // float moveVertical = Input.GetAxisRaw("LeftJoyStickVertical");
-        transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime);
+        transform.Translate(Input.GetAxis(inputs[0]) * speed * Time.deltaTime, 0, Input.GetAxis(inputs[1]) * speed * Time.deltaTime);
     }
 }
